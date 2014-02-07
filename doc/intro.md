06fc218f6c0524ba90399fce05142b7eaa72c6cd
Steven McCord <steven.a.mccord@gmail.com>
2014-02-07 08:54:41 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to learningclj
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/learningclj/core_test.clj
@@ -0,0 +1,7 @@
+(ns learningclj.core-test
+  (:require [clojure.test :refer :all]
+            [learningclj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
